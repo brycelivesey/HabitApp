b8e455cd2164e7749ae4ec778651114ea2a6dabe
Bryce Livesey <liveseybr@gmail.com>
2025-02-09 23:42:11 -0500

    Add MongoDB index initialization for Users and Goals collections

--- Src/Program.cs
+++ Src/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<IGoalService, GoalService>();
 builder.Services.AddScoped<IGoalRepository, GoalRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IMongoDbInitializer, MongoDbInitializer>();
 
 builder.Services.AddSingleton<IJwtService, JwtService>();
 
@@ -44,14 +45,19 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+// Initialize MongoDB indexes
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<IMongoDbInitializer>();
+    await initializer.InitializeAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
