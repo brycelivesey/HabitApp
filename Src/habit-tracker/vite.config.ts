307960f9a2c47ac8c10255f2f683c84b46ec64ec
Bryce Livesey <liveseybr@gmail.com>
2025-03-13 19:48:22 -0400

    msbuild to auto build the frontend

--- Src/habit-tracker/vite.config.ts
+++ Src/habit-tracker/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../wwwroot',
+    // outDir: 'dist',
     emptyOutDir: true,
     manifest: true,
     // Might need these for .NET integration
