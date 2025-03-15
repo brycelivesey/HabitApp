ee7d9b60982ae527b3c2b2b3e9a13a97bd2637c4
Bryce Livesey <liveseybr@gmail.com>
2025-03-14 21:14:45 -0400

    unused use state

--- Src/habit-tracker/src/App.tsx
+++ Src/habit-tracker/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
